eaf2081145474384120e80a4d375670df7e651fc
Helen <dd040388kev@gmail.com>
2022-03-31 12:50:11 +0300

    +10

--- js/task-10.js
+++ js/task-10.js
@@ -1,16 +1,36 @@
-// Напиши скрипт создания и очистки коллекции элементов.Пользователь вводит количество элементов в input 
-// и нажимает кнопку Создать, после чего рендерится коллекция. 
-// При нажатии на кнопку Очистить, коллекция элементов очищается.
-
-// Создай функцию createBoxes(amount), которая принимает один параметр - число.Функция создает столько < div >,
-// сколько указано в amount и добавляет их в div#boxes.
-// 1. Размеры самого первого <div> - 30px на 30px.
-// 2. Каждый элемент после первого, должен быть шире и выше предыдущего на 10px.
-// 3. Все элементы должены иметь случайный цвет фона в формате HEX. 
-// Используй готовую функцию getRandomHexColor для получения цвета.
-
-// Создай функцию destroyBoxes(), которая очищает содержимое div#boxes, тем самым удаляя все созданные элементы.
-
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const boxesParent = document.querySelector("#boxes");
+const btnCreateEl = document.querySelector("[data-create]");
+const btnDestroyEl = document.querySelector("[data-destroy]");
+const inputEl = document.querySelector("#controls input");
+let initialSizeOfBox = 30;
+
+const createBtnListener = () => {
+  const amountOfBoxes = createBoxes(inputEl.value);
+  boxesParent.append(...amountOfBoxes);
+}
+
+const createBoxes = (amount) => {
+  let sizeOfBox = 0;
+  const boxElements = [];
+  for (let i = 0; i < amount; i += 1) {
+    const boxEl = document.createElement("div");
+    boxEl.style.backgroundColor = getRandomHexColor();
+    sizeOfBox = initialSizeOfBox + i * 10;
+    boxEl.style.width = sizeOfBox + "px";
+    boxEl.style.height = sizeOfBox + "px";
+    boxElements.push(boxEl);
+  }
+  initialSizeOfBox = sizeOfBox + 10;
+  return boxElements;
+};
+
+const destroyBoxes = () => {
+  boxesParent.remove();
+};
+
+btnCreateEl.addEventListener("click", createBtnListener);
+btnDestroyEl.addEventListener("click", destroyBoxes);
